14ba789c73ec5a4243f8f212d4bde90e74bf4dcd
yus-ra17 <mohammedyusra407@gmail.com>
2024-07-09 22:20:18 +0300

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Home from "./Pages/Home/Home";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Home />
     </div>
   );
 }
---
+++ netflix-clone/src/Components/Footer/Footer.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "./footer.css";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+
+function Footer() {
+  return (
+    <div className="footerContainer">
+      <div className="foot">
+        <div className="icons">
+          <ul>
+            <li>
+              <FacebookOutlinedIcon />
+            </li>
+            <li>
+              <InstagramIcon />
+            </li>
+            <li>
+              <YouTubeIcon />
+            </li>
+          </ul>
+        </div>
+        <ul>
+          <li>Audio Description</li>
+          <li>Investor Relations</li>
+          <li>Legal Notice</li>
+          <li>Service Code</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <li>Help Center</li>
+          <li>Jobs</li>
+          <li>Cokkie Preferences</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <li>Gift Cards</li>
+          <li>Terms of Use</li>
+          <li>Corporate Information</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <li>Media Center</li>
+          <li>Privacy</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+
+      <div className="service">
+        <p>Service Code</p>
+      </div>
+
+      <div>&copy; 1997-2024 Netflix, Inc.</div>
+    </div>
+  );
+}
+
+export default Footer;
